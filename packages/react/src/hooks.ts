20ba319f8490a215471c1c5f0b04775f47756cba
Yuki Iwanaga <yuki@creasty.com>
2025-01-11 20:48:11 +0900

    Tweak

--- packages/react/src/hooks.ts
+++ packages/react/src/hooks.ts
@@ -7,7 +7,7 @@ import { useEffect } from "react";
  * The difference from `Form.get` is that this hook automatically resets the form
  * when the component is mounted and unmounted.
  *
- * @inheritdoc {@link Form.get}
+ * @inheritdoc {@link @form-model/core!Form.get}
  */
 export function useForm<T extends object>(subject: T, opt?: { formKey?: symbol; noReset?: boolean }): Form<T> {
   const form = Form.get(subject, opt?.formKey);
