76bfa0fbfcde39294fe353aa921f4ece6c3922b4
Yuki Iwanaga <yuki@creasty.com>
2025-01-11 13:51:01 +0900

    Export binding classes and separate import of extension

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,5 @@
-import "./extension";
+export { CheckBoxBinding } from "./CheckBoxBinding";
+export { InputBinding } from "./InputBinding";
+export { RadioButtonBinding } from "./RadioButtonBinding";
+export { SelectBoxBinding } from "./SelectBoxBinding";
+export { SubmitButtonBinding } from "./SubmitButtonBinding";
