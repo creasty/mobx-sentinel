91cc1370f33b9ad7d3a6707a80d181f1868f0589
Yuki Iwanaga <yuki@creasty.com>
2025-01-15 19:28:34 +0900

    Test case for merging results

--- packages/core/src/validation.test.ts
+++ packages/core/src/validation.test.ts
@@ -329,28 +329,76 @@ describe("Validation", () => {
 });
 
 describe("toErrorMap", () => {
-  it("converts a validation result to an error map", () => {
-    const result = {
-      null: null,
-      undefined: undefined,
-      string: "string",
-      error: new Error("error"),
-      array: ["string", new Error("error")],
-    };
-    const map = toErrorMap([result]);
-    expect(map).toMatchInlineSnapshot(`
-      Map {
-        "string" => [
-          "string",
-        ],
-        "error" => [
-          "error",
-        ],
-        "array" => [
-          "string",
-          "error",
-        ],
-      }
-    `);
+  describe("single result", () => {
+    it("ignores null and undefined values", () => {
+      expect(toErrorMap([{ field1: null, field2: undefined }])).toEqual(new Map());
+    });
+
+    it("ignores empty strings", () => {
+      expect(toErrorMap([{ field1: "" }])).toEqual(new Map());
+    });
+
+    it("ignores empty arrays", () => {
+      expect(toErrorMap([{ field1: [] }])).toEqual(new Map());
+    });
+
+    it("works with a string", () => {
+      expect(toErrorMap([{ field1: "error" }])).toEqual(new Map([["field1", ["error"]]]));
+    });
+
+    it("works with an error", () => {
+      expect(toErrorMap([{ field1: new Error("error") }])).toEqual(new Map([["field1", ["error"]]]));
+    });
+
+    it("works with an array of strings", () => {
+      expect(toErrorMap([{ field1: ["error1", "error2"] }])).toEqual(new Map([["field1", ["error1", "error2"]]]));
+    });
+
+    it("works with an array of errors", () => {
+      expect(toErrorMap([{ field1: [new Error("error1"), new Error("error2")] }])).toEqual(
+        new Map([["field1", ["error1", "error2"]]])
+      );
+    });
+
+    it("works with an array of strings and errors", () => {
+      expect(toErrorMap([{ field1: ["error1", new Error("error2")] }])).toEqual(
+        new Map([["field1", ["error1", "error2"]]])
+      );
+    });
+  });
+
+  describe("multiple results", () => {
+    it("does not invalidate the result with null or undefined values", () => {
+      expect(toErrorMap([{ field1: "error" }, { field1: null }, { field1: undefined }])).toEqual(
+        new Map([["field1", ["error"]]])
+      );
+    });
+
+    it("merges a string and an error", () => {
+      expect(toErrorMap([{ field1: "error" }, { field1: new Error("error2") }])).toEqual(
+        new Map([["field1", ["error", "error2"]]])
+      );
+    });
+
+    it("merges non-arrays and arrays", () => {
+      expect(toErrorMap([{ field1: "error" }, { field1: ["error2", "error3"] }])).toEqual(
+        new Map([["field1", ["error", "error2", "error3"]]])
+      );
+    });
+
+    it("merges an array of strings and an array of errors", () => {
+      expect(
+        toErrorMap([{ field1: ["error1", "error2"] }, { field1: [new Error("error3"), new Error("error4")] }])
+      ).toEqual(new Map([["field1", ["error1", "error2", "error3", "error4"]]]));
+    });
+
+    it("merges multiple fields", () => {
+      expect(toErrorMap([{ field1: "error1" }, { field1: "error3", field2: "error4" }])).toEqual(
+        new Map([
+          ["field1", ["error1", "error3"]],
+          ["field2", ["error4"]],
+        ])
+      );
+    });
   });
 });
