e2308be8b743958e72b6ca12d6b365db5676532f
Yuki Iwanaga <yuki@creasty.com>
2025-03-06 14:31:10 +0900

    Unexport getNestedAnnotations

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,4 @@
-export { nested, getNestedAnnotations, StandardNestedFetcher } from "./nested";
+export { nested, StandardNestedFetcher } from "./nested";
 export { Watcher, watch, unwatch } from "./watcher";
 export { Validator, makeValidatable } from "./validator";
 export type { ValidationError, ValidationErrorMapBuilder } from "./error";
