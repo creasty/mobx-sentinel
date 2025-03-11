9e4aa90714d2cc9c870b7af13bb946c808833154
Yuki Iwanaga <yuki@creasty.com>
2025-03-11 21:08:20 +0900

    Add decorator info

--- script/typedoc-plugin.js
+++ script/typedoc-plugin.js
@@ -1,18 +1,21 @@
 import * as td from "typedoc";
 
-/** @param {td.Application} app */
+const ts = td.TypeScript;
+
+/**
+ * @param {td.Application} app
+ */
 export function load(app) {
   app.converter.on(td.Converter.EVENT_END, (context) => {
-    sort(context.project);
+    clusterSameNameEntries(context.project);
   });
+  app.converter.on(td.Converter.EVENT_CREATE_DECLARATION, addDecoratorInfo);
 }
 
 /**
- * Sort children of the given container.
- *
- * @param container {td.ContainerReflection}
+ * @param {td.ContainerReflection} container
  */
-function sort(container) {
+function clusterSameNameEntries(container) {
   if (
     !container.kindOf(
       td.ReflectionKind.Project | td.ReflectionKind.Module | td.ReflectionKind.Namespace | td.ReflectionKind.Class
@@ -26,7 +29,6 @@ function sort(container) {
   /** @type WeakMap<td.DeclarationReflection, number> */
   const orderMap = new WeakMap();
 
-  // Group children with the same name together
   for (let i = 0, children = container.children ?? [], len = children.length; i < len; i++) {
     const child = children[i];
     const key = child.getFullName();
@@ -40,12 +42,62 @@ function sort(container) {
     const penalty = child.kindOf(td.ReflectionKind.Namespace) ? 0.1 : 0;
     // Keep the original order of children consistent
     const localOrder = i / len / 10;
-    // Move the children with the same name closer together
+    // Fractional indexing
     orderMap.set(child, canonicalOrder + localOrder + penalty);
 
     // Recurse into children
-    if (child.children) sort(child);
+    if (child.children) clusterSameNameEntries(child);
   }
 
   container.children.sort((a, b) => orderMap.get(a) - orderMap.get(b));
+}
+
+/**
+ * @param {td.Context} context
+ * @param {td.DeclarationReflection} decl
+ *
+ * @see https://github.com/TypeStrong/typedoc/issues/2346
+ */
+function addDecoratorInfo(context, decl) {
+  const symbol = context.getSymbolFromReflection(decl);
+  if (!symbol) return;
+
+  const declaration = symbol.valueDeclaration;
+  if (!declaration) return;
+  if (
+    !ts.isPropertyDeclaration(declaration) &&
+    !ts.isMethodDeclaration(declaration) &&
+    !ts.isGetAccessorDeclaration(declaration)
+  ) {
+    return;
+  }
+
+  const decorators = declaration.modifiers?.filter(ts.isDecorator);
+  for (const decorator of decorators ?? []) {
+  const expr = decorator.getText().split("(", 1)[0];
+    const modifierTags = [];
+    switch (expr) {
+      case "@action":
+      case "@action.bound": {
+        modifierTags.push(expr);
+        break;
+      }
+      case "@computed":
+      case "@computed.struct": {
+        modifierTags.push("@computed");
+        break;
+      }
+      default: {
+        console.warn("Unknown decorator:", expr);
+        break;
+      }
+    }
+    if (modifierTags.length > 0) {
+      decl.comment ??= new td.Comment();
+      for (const tag of modifierTags) {
+        decl.comment.modifierTags.add(tag);
+      }
+      //console.log('Add decorator tag:', decl.getFullName(), modifierTags);
+    }
+  }
 }
